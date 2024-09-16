076db21d62dfe1abc41f25f539c96b79d641f66d
YeYint009 <blackskym96@gmail.com>
2024-09-17 01:59:45 +0900

    feat/monthlyIncomeEdit

--- src/app/page/dashboard/components/Monthly.tsx
+++ src/app/page/dashboard/components/Monthly.tsx
@@ -1,28 +1,67 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PencilIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const Monthly = () => {
+  const [openDialog, setOpenDialog] = useState(false);
   return (
     <Card className="shadow-lg ">
       <CardHeader className="flex flex-row items-center justify-between -space-y-3 pb-2">
-        <CardTitle className=" fond-bold text-md whitespace-nowrap">Monthly Income</CardTitle>
+        <CardTitle className=" fond-bold text-md whitespace-nowrap">
+          Monthly Income
+        </CardTitle>
         <Dialog>
           <DialogTrigger asChild>
             <Button variant="ghost" size="icon" className="h-4 w-4">
-                <PencilIcon className="w-4 h-4" />
+              <PencilIcon className="w-4 h-4" />
             </Button>
           </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
         </Dialog>
       </CardHeader>
       <CardContent className="flex items-center">
